0a7973bfd32a9c2b30712081308587c0908239ea
Fredohu <97898567+Fredohu@users.noreply.github.com>
2022-06-22 02:27:01 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+import { createAlchemyWeb3 } from '@alch/alchemy-web3';
+export const NFTCONTRACT = "0xf93099D8BD170F347941f5b8e29275c61B5009e8";
+export const STAKINGCONTRACT = "0xdeca8696Ecbc1BB6798bd9efCABD0982c7326c32";
+export const bscscanapi = "https://api.bscscan.com/api";
+export const moralisapi = "https://deep-index.moralis.io/api/v2/";
+export const nftpng = "https://apehybrids.mypinata.cloud/ipfs/QmbzhS3xuwMBZSWyUfuyVVAPzg2Ngpxtjh3DSneqT8cvhQ/";
+export const Web3Alc = createAlchemyWeb3("https://speedy-nodes-nyc.moralis.io/36e16d2ef26dda90a067fe0e/bsc/mainnet");
